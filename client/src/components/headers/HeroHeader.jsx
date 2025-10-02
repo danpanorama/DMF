1730e600353509b2ea1107ccd113872e7bbf7e8c
danpanorama <dani3morcos@gmail.com>
2025-10-02 19:14:34 +0300

    add meta to index HTML

--- client/src/components/headers/HeroHeader.jsx
+++ client/src/components/headers/HeroHeader.jsx
@@ -1,12 +1,10 @@
 import '../../css/headers.css';
 
-
-
 function HeroHeader() {
   return (
     <div className="heroHeaderContainer">
-    <h1 className='heroHeaderH1'>מלווים אותכם להשקעה בטוחה !</h1>
-    
+      <h1 className='heroHeaderH1'>Empowering Your Safe Investments!</h1>
+      <p className='heroHeaderSubtext'>Guiding you step by step towards financial success.</p>
     </div>
   );
 }

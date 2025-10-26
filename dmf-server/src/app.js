2f871454a9286e8554863d4f5e2f007d72b8643c
danpanorama <dani3morcos@gmail.com>
2025-10-26 12:59:20 +0200

    'health'

--- dmf-server/src/app.js
+++ dmf-server/src/app.js
@@ -25,6 +25,7 @@ app.use(helmet());
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
+app.get('/healthz', (req, res) => res.sendStatus(200));
 
 // app.use(mongoSanitize());
 

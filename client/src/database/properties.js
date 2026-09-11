3201ee7520af0ab6677432922bc05b288bb3ae73
danpanorama <dani3morcos@gmail.com>
2026-09-11 17:49:39 +0300

    photo bag fix

--- client/src/database/properties.js
+++ client/src/database/properties.js
@@ -1,3 +1,83 @@
+// const properties = [
+//   {
+//     id: 'ano-poli-thessaloniki',
+//     name: 'Apartment in Ano Poli Thessaloniki',
+//     description:
+//       'Beautifully renovated 3-bedroom apartment with 1 bathroom, located in Ano Poli, Thessaloniki.',
+//     location: 'City Center – Ano Poli',
+//     rooms: 3,
+//     bathrooms: 1,
+//     floor: '2',
+//     size: 67,
+//     address: 'Sachini 14, Thessaloniki 546 32',
+//     images: [
+//       '../../public/images/sachini/2.png',
+//       '../../public/images/sachini/1.png',
+//       '../../public/images/sachini/3.png',
+//       '../../public/images/sachini/4.png',
+//       '../../public/images/sachini/5.png',
+//       '../../public/images/sachini/6.png',
+//       '../../public/images/sachini/7.png',
+//     ],
+//     businessPotential:
+//       '💶 Long-term rent: €650–€700/month. Airbnb daily rate: €87–€115.',
+//     price: 90000,
+//   },
+
+//   {
+//     id: 'karaiskaki-thessaloniki',
+//     name: 'Newly Renovated Apartment',
+//     description:
+//       'Fully renovated 3-room apartment, 52 sqm, on the ground floor.',
+//     location: 'Near the sea and university area',
+//     rooms: 3,
+//     bathrooms: 1,
+//     floor: 'G',
+//     size: 52,
+//     address: 'Karaiskaki 19, Thessaloniki 546 41',
+//     images: [
+//       '../../public/images/karaskali/kar1.jpeg',
+//       '../../public/images/karaskali/kar2.jpeg',
+//       '../../public/images/karaskali/kar3.jpeg',
+//       '../../public/images/karaskali/kar4.jpeg',
+//       '../../public/images/karaskali/kar5.jpeg',
+//       '../../public/images/karaskali/kar6.jpeg',
+//     ],
+//     businessPotential:
+//       '💶 Long-term rent: €400–€500/month. Popular with students.',
+//     price: 84000,
+//   },
+
+//   {
+//     id: 'strati-mirivili-thessaloniki',
+//     name: 'Modern Apartment',
+//     description:
+//       'Newly renovated 3-bedroom apartment with 1 bathroom, 76 sqm, on the 2nd floor.',
+//     location: 'Quiet and historic neighborhood',
+//     rooms: 3,
+//     bathrooms: 1,
+//     floor: '2',
+//     size: 76,
+//     address: 'Strati Mirivili 11, Thessaloniki 546 32',
+//     images: [
+//       '../../public/images/merivily/merbalcony.jpeg',
+//       '../../public/images/merivily/mertoilet.jpeg',
+//       '../../public/images/merivily/merrooms.png',
+//       '../../public/images/merivily/mer.png',
+//       '../../public/images/merivily/merbalcony.jpeg',
+//       '../../public/images/merivily/merenter.jpeg',
+//       '../../public/images/merivily/merkitchen.jpeg',
+//       '../../public/images/merivily/merenter.png',
+//       '../../public/images/merivily/merroom.jpeg',
+//     ],
+//     businessPotential:
+//       '💶 Long-term rent: €700–€850/month. Airbnb daily rate: €80–€100.',
+//     price: 95000,
+//   },
+// ];
+
+// export default properties;
+
 const properties = [
   {
     id: 'ano-poli-thessaloniki',
@@ -11,13 +91,13 @@ const properties = [
     size: 67,
     address: 'Sachini 14, Thessaloniki 546 32',
     images: [
-      '../../public/images/sachini/2.png',
-      '../../public/images/sachini/1.png',
-      '../../public/images/sachini/3.png',
-      '../../public/images/sachini/4.png',
-      '../../public/images/sachini/5.png',
-      '../../public/images/sachini/6.png',
-      '../../public/images/sachini/7.png',
+      '/images/sachini/2.png',
+      '/images/sachini/1.png',
+      '/images/sachini/3.png',
+      '/images/sachini/4.png',
+      '/images/sachini/5.png',
+      '/images/sachini/6.png',
+      '/images/sachini/7.png',
     ],
     businessPotential:
       '💶 Long-term rent: €650–€700/month. Airbnb daily rate: €87–€115.',
@@ -36,12 +116,12 @@ const properties = [
     size: 52,
     address: 'Karaiskaki 19, Thessaloniki 546 41',
     images: [
-      '../../public/images/karaskali/kar1.jpeg',
-      '../../public/images/karaskali/kar2.jpeg',
-      '../../public/images/karaskali/kar3.jpeg',
-      '../../public/images/karaskali/kar4.jpeg',
-      '../../public/images/karaskali/kar5.jpeg',
-      '../../public/images/karaskali/kar6.jpeg',
+      '/images/karaskali/kar1.jpeg',
+      '/images/karaskali/kar2.jpeg',
+      '/images/karaskali/kar3.jpeg',
+      '/images/karaskali/kar4.jpeg',
+      '/images/karaskali/kar5.jpeg',
+      '/images/karaskali/kar6.jpeg',
     ],
     businessPotential:
       '💶 Long-term rent: €400–€500/month. Popular with students.',
@@ -60,15 +140,15 @@ const properties = [
     size: 76,
     address: 'Strati Mirivili 11, Thessaloniki 546 32',
     images: [
-      '../../public/images/merivily/merbalcony.jpeg',
-      '../../public/images/merivily/mertoilet.jpeg',
-      '../../public/images/merivily/merrooms.png',
-      '../../public/images/merivily/mer.png',
-      '../../public/images/merivily/merbalcony.jpeg',
-      '../../public/images/merivily/merenter.jpeg',
-      '../../public/images/merivily/merkitchen.jpeg',
-      '../../public/images/merivily/merenter.png',
-      '../../public/images/merivily/merroom.jpeg',
+      '/images/merivily/merbalcony.jpeg',
+      '/images/merivily/mertoilet.jpeg',
+      '/images/merivily/merrooms.png',
+      '/images/merivily/mer.png',
+      '/images/merivily/merbalcony.jpeg',
+      '/images/merivily/merenter.jpeg',
+      '/images/merivily/merkitchen.jpeg',
+      '/images/merivily/merenter.png',
+      '/images/merivily/merroom.jpeg',
     ],
     businessPotential:
       '💶 Long-term rent: €700–€850/month. Airbnb daily rate: €80–€100.',

4ca2db2ca16a68d2415c67a80dbdf8169ca2f58f
danpanorama <dani3morcos@gmail.com>
2026-09-13 15:57:20 +0300

    data change

--- client/src/database/productData.js
+++ client/src/database/productData.js
@@ -1,114 +1,114 @@
-// // src/components/products/productsData.js
-const Sachini1 = "https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800316/sachinibaniyo_hy92aq.png";
-const Sachini2 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800313/sachinicoozina_vizuyf.png';
-const Sachini3 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800313/sachiniroom1middle_pcs0tw.png';
-const Sachini4 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800314/sachiniroom1port_zjtsip.png';
-const Sachini5 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800315/sachiniroom2front_u9lzrs.png';
-const Sachini6 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800311/sachiniroom2port_qsjdmy.png';
-const Sachini7 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800315/sachiniroom2rik_bwy9af.png';
+// // // src/components/products/productsData.js
+// const Sachini1 = "https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800316/sachinibaniyo_hy92aq.png";
+// const Sachini2 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800313/sachinicoozina_vizuyf.png';
+// const Sachini3 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800313/sachiniroom1middle_pcs0tw.png';
+// const Sachini4 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800314/sachiniroom1port_zjtsip.png';
+// const Sachini5 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800315/sachiniroom2front_u9lzrs.png';
+// const Sachini6 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800311/sachiniroom2port_qsjdmy.png';
+// const Sachini7 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1760800315/sachiniroom2rik_bwy9af.png';
 
-const mer1 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133868/merbalcony_kh8uxa.png';
-const mer2 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133869/merenter_elaqqg.png';
-const mer3 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133837/mer_fhnyko.png';
-const mer4 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133869/merkitchen_f1ilsg.png';
-const mer5 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133871/merout_oxomb0.png';
-const mer6 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133871/merroom_myyana.png';
-const mer7 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133865/merrooms_wfqv6o.png';
-const mer8 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133866/merrroom_qdqdii.png';
-const mer9 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133866/mertoilet_pow1tn.png';
+// const mer1 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133868/merbalcony_kh8uxa.png';
+// const mer2 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133869/merenter_elaqqg.png';
+// const mer3 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133837/mer_fhnyko.png';
+// const mer4 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133869/merkitchen_f1ilsg.png';
+// const mer5 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133871/merout_oxomb0.png';
+// const mer6 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133871/merroom_myyana.png';
+// const mer7 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133865/merrooms_wfqv6o.png';
+// const mer8 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133866/merrroom_qdqdii.png';
+// const mer9 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761133866/mertoilet_pow1tn.png';
 
-const kar1 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134239/kar1_lgyhwq.jpg';
-const kar2 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134240/kar2_ksulyk.jpg';
-const kar3 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134241/kar3_n7rjce.jpg';
-const kar4 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134244/kar4_wenbau.jpg';
-const kar5 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134246/kar5_kah7ih.jpg';
-const kar6 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134247/kar6_qzlpuz.jpg';
+// const kar1 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134239/kar1_lgyhwq.jpg';
+// const kar2 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134240/kar2_ksulyk.jpg';
+// const kar3 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134241/kar3_n7rjce.jpg';
+// const kar4 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134244/kar4_wenbau.jpg';
+// const kar5 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134246/kar5_kah7ih.jpg';
+// const kar6 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134247/kar6_qzlpuz.jpg';
 
-const kar21 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134373/kar21_cxkjg2.jpg';
-const kar22 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134376/kar22_lhqjdx.jpg';
-const kar23 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134391/kar223_jpnfpx.jpg';
-const kar24 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134378/kar24_oqfhlq.jpg';
-const kar25 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134380/kar25_pqlfsk.jpg';
-const kar26 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134381/kar26_jqej7d.jpg';
-const kar27 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134383/kar27_mlfj6g.jpg';
-const kar28 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134385/kar28_fm3ihv.jpg';
-const kar29 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134387/kar29_mi0vjb.jpg';
-const kar210 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134389/kar210_on0nbf.jpg';
-
-
+// const kar21 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134373/kar21_cxkjg2.jpg';
+// const kar22 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134376/kar22_lhqjdx.jpg';
+// const kar23 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134391/kar223_jpnfpx.jpg';
+// const kar24 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134378/kar24_oqfhlq.jpg';
+// const kar25 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134380/kar25_pqlfsk.jpg';
+// const kar26 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134381/kar26_jqej7d.jpg';
+// const kar27 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134383/kar27_mlfj6g.jpg';
+// const kar28 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134385/kar28_fm3ihv.jpg';
+// const kar29 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134387/kar29_mi0vjb.jpg';
+// const kar210 = 'https://res.cloudinary.com/dqdwesvsg/image/upload/v1761134389/kar210_on0nbf.jpg';
 
 
-export const products = [
-  {
-    id: 1,
-    name: "Apartment in Ano Poli Thessaloniki",
-    description:
-      "Beautifully renovated 3-bedroom apartment with 1 bathroom, located on the 2nd floor (67 sqm). The apartment is fully equipped with modern A/C units for both cooling and heating. Includes new kitchen, stylish bathroom, and bright living area with natural light throughout. Ideal for families or investors looking for a ready-to-rent unit in the heart of Thessaloniki.",
-    location: "City Center – Ano Poli",
-    rooms: 3,
-    bathrooms: 1,
-    floor: 2,
-    size: 67,
-    address: "Sachini 14, Thessaloniki 546 32",
-    price: 90000,
-    energyClass: "B",
-    heatingCooling: "Full A/C system (hot & cold)",
-    images: [Sachini2, Sachini5, Sachini1, Sachini3, Sachini4, Sachini6, Sachini7],
-    businessPotential:
-      "💶 Long-term rent: €650–€700/month. Airbnb daily rate: €87–€115, occupancy 75%. Estimated ROI: 6–7% yearly. Excellent appreciation potential due to its proximity to the sea and main shopping streets.",
-  },
-  {
-    id: 2,
-    name: "Modern Apartment",
-    description:
-      "Newly renovated 3-bedroom apartment with 1 bathroom, 76 sqm, on the 2nd floor. Bright and modern design featuring a large balcony with a city view. Fully equipped with A/C systems for both hot and cold air. Located in a quiet, historic neighborhood with easy access to cafes and public transportation.",
-    location: "Quiet and historic neighborhood",
-    rooms: 3,
-    bathrooms: 1,
-    floor: 2,
-    size: 76,
-    address: "Strati Mirivili 11, Thessaloniki 546 32",
-    price: 95000,
-    energyClass: "B",
-    heatingCooling: "Full A/C system (hot & cold)",
-    images: [mer1, mer2,mer3,mer4,mer5,mer6,mer7,mer8,mer9],
-    businessPotential:
-      "💶 Long-term rent: €700–€850/month. Airbnb daily rate: €80–€100, occupancy 70%. ROI: 5.5–6%. Solid appreciation expected due to demand in this desirable area.",
-  },
-  {
-    id: 3,
-    name: "Newly Renovated Apartment",
-    description:
-      "Fully renovated 3-room apartment (52 sqm) on the ground floor. Perfect for students or short-term rentals. Equipped with efficient A/C system for both heating and cooling. Located just steps away from the university and public transportation. Turnkey condition – no renovations needed.",
-    location: "Near the sea and university area",
-    rooms: 3,
-    bathrooms: 1,
-    floor: "G",
-    size: 52,
-    address: "Karaiskaki 19, Thessaloniki 546 41",
-    price: 84000,
-    energyClass: "B",
-    heatingCooling: "Full A/C system (hot & cold)",
-    images: [kar1, kar2, kar3,kar4,kar5,kar6],
-    businessPotential:
-      "💶 Long-term rent: €400–€500/month (popular with students). Airbnb daily rate: €40–€60, occupancy 80%+. ROI: 7–8% yearly. Strong cash-flow investment with minimal maintenance.",
-  },
-  {
-    id: 4,
-    name: "Beautiful Near See Apartment",
-    description:
-      "2-bedroom Near See Apartment (50 sqm) with 1 bathroom and private terrace. Features an A-class energy system with natural gas heating and A/C cooling. Recently refurbished with elegant finishes and modern insulation for maximum efficiency. Located in a high-end area with excellent accessibility.",
-    location: "High-end neighborhood",
-    rooms: 2,
-    bathrooms: 1,
-    floor: "G",
-    size: 50,
-    address: "Karaiskaki 19, Thessaloniki 546 41",
-    price: 55000,
-    energyClass: "A",
-    heatingCooling: "Natural gas heating + A/C cooling",
-    images: [kar23,kar21,kar210, kar22,kar24,kar25,kar26,kar27,kar28,kar29],
-    businessPotential:
-      "💶 Long-term rent: €350–€450/month. Airbnb daily rate: €180–€250, occupancy 65%. ROI: ~6%+. Excellent value appreciation expected for this rare sea-view property.",
-  },
-];
+
+
+// export const products = [
+//   {
+//     id: 1,
+//     name: "Apartment in Ano Poli Thessaloniki",
+//     description:
+//       "Beautifully renovated 3-bedroom apartment with 1 bathroom, located on the 2nd floor (67 sqm). The apartment is fully equipped with modern A/C units for both cooling and heating. Includes new kitchen, stylish bathroom, and bright living area with natural light throughout. Ideal for families or investors looking for a ready-to-rent unit in the heart of Thessaloniki.",
+//     location: "City Center – Ano Poli",
+//     rooms: 3,
+//     bathrooms: 1,
+//     floor: 2,
+//     size: 67,
+//     address: "Sachini 14, Thessaloniki 546 32",
+//     price: 90000,
+//     energyClass: "B",
+//     heatingCooling: "Full A/C system (hot & cold)",
+//     images: [Sachini2, Sachini5, Sachini1, Sachini3, Sachini4, Sachini6, Sachini7],
+//     businessPotential:
+//       "💶 Long-term rent: €650–€700/month. Airbnb daily rate: €87–€115, occupancy 75%. Estimated ROI: 6–7% yearly. Excellent appreciation potential due to its proximity to the sea and main shopping streets.",
+//   },
+//   {
+//     id: 2,
+//     name: "Modern Apartment",
+//     description:
+//       "Newly renovated 3-bedroom apartment with 1 bathroom, 76 sqm, on the 2nd floor. Bright and modern design featuring a large balcony with a city view. Fully equipped with A/C systems for both hot and cold air. Located in a quiet, historic neighborhood with easy access to cafes and public transportation.",
+//     location: "Quiet and historic neighborhood",
+//     rooms: 3,
+//     bathrooms: 1,
+//     floor: 2,
+//     size: 76,
+//     address: "Strati Mirivili 11, Thessaloniki 546 32",
+//     price: 95000,
+//     energyClass: "B",
+//     heatingCooling: "Full A/C system (hot & cold)",
+//     images: [mer1, mer2,mer3,mer4,mer5,mer6,mer7,mer8,mer9],
+//     businessPotential:
+//       "💶 Long-term rent: €700–€850/month. Airbnb daily rate: €80–€100, occupancy 70%. ROI: 5.5–6%. Solid appreciation expected due to demand in this desirable area.",
+//   },
+//   {
+//     id: 3,
+//     name: "Newly Renovated Apartment",
+//     description:
+//       "Fully renovated 3-room apartment (52 sqm) on the ground floor. Perfect for students or short-term rentals. Equipped with efficient A/C system for both heating and cooling. Located just steps away from the university and public transportation. Turnkey condition – no renovations needed.",
+//     location: "Near the sea and university area",
+//     rooms: 3,
+//     bathrooms: 1,
+//     floor: "G",
+//     size: 52,
+//     address: "Karaiskaki 19, Thessaloniki 546 41",
+//     price: 84000,
+//     energyClass: "B",
+//     heatingCooling: "Full A/C system (hot & cold)",
+//     images: [kar1, kar2, kar3,kar4,kar5,kar6],
+//     businessPotential:
+//       "💶 Long-term rent: €400–€500/month (popular with students). Airbnb daily rate: €40–€60, occupancy 80%+. ROI: 7–8% yearly. Strong cash-flow investment with minimal maintenance.",
+//   },
+//   {
+//     id: 4,
+//     name: "Beautiful Near See Apartment",
+//     description:
+//       "2-bedroom Near See Apartment (50 sqm) with 1 bathroom and private terrace. Features an A-class energy system with natural gas heating and A/C cooling. Recently refurbished with elegant finishes and modern insulation for maximum efficiency. Located in a high-end area with excellent accessibility.",
+//     location: "High-end neighborhood",
+//     rooms: 2,
+//     bathrooms: 1,
+//     floor: "G",
+//     size: 50,
+//     address: "Karaiskaki 19, Thessaloniki 546 41",
+//     price: 55000,
+//     energyClass: "A",
+//     heatingCooling: "Natural gas heating + A/C cooling",
+//     images: [kar23,kar21,kar210, kar22,kar24,kar25,kar26,kar27,kar28,kar29],
+//     businessPotential:
+//       "💶 Long-term rent: €350–€450/month. Airbnb daily rate: €180–€250, occupancy 65%. ROI: ~6%+. Excellent value appreciation expected for this rare sea-view property.",
+//   },
+// ];
